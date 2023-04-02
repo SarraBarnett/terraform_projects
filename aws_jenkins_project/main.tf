e4ee0cbf8075b689de1858d74bfaa1dc87f6d2fa
SarraBarnett <sarrabarnett@gmail.com>
2023-04-02 19:58:16 +0000

    edited script

--- aws_jenkins_project/main.tf
+++ aws_jenkins_project/main.tf
@@ -42,26 +42,30 @@ resource "aws_security_group" "ec2_jenkins" {
   vpc_id      = "vpc-03b53edf3579ce9c1"
 
   ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 443
-    to_port   = 443
-    protocol  = "tcp"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port = 8080
-    to_port   = 8080
-    protocol  = "tcp"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
